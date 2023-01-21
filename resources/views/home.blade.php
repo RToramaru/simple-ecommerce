6296f0273bb98753aa57078bf683e7c64ec3841f
Rafael Almeida Soares <rafael.almeida.soares2012@hotmail.com>
2023-01-21 20:20:18 -0300

    criacao do projeto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RToramaru Shopping</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css" rel="stylesheet">
+</head>
+<body>
+    
+    <nav class="navbar navbar-light navbar-expand-md bg-light pl-5 pr-5 mb-5">
+        <a href="#" class="navbar-brand">RToramaru Shopping</a>
+        <div class="collapse navbar-collapse">
+            <div class="navbar-nav">
+                <a href="#" class="nav-link">Home</a>
+                <a href="#" class="nav-link">Categorias</a>
+                <a href="#" class="nav-link">Cadastrar</a>
+            </div>
+        </div>
+        <a href="#" class="btn btn-sm"><i class="bi bi-cart"></i></a>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-3 mb-3">
+                <div class="card">
+                    <img src="/img/produto_1.jpg" class="card-img-top">
+                    <div class="card-body">
+                        <h6 class="card-title">Produto 1</h6>
+                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3 mb-3">
+                <div class="card">
+                    <img src="/img/produto_2.jpg" class="card-img-top">
+                    <div class="card-body">
+                        <h6 class="card-title">Produto 1</h6>
+                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3 mb-3">
+                <div class="card">
+                    <img src="/img/produto_3.jpg" class="card-img-top">
+                    <div class="card-body">
+                        <h6 class="card-title">Produto 1</h6>
+                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3 mb-3">
+                <div class="card">
+                    <img src="/img/produto_4.png" class="card-img-top">
+                    <div class="card-body">
+                        <h6 class="card-title">Produto 1</h6>
+                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3 mb-3">
+                <div class="card">
+                    <img src="/img/produto_5.jpg" class="card-img-top">
+                    <div class="card-body">
+                        <h6 class="card-title">Produto 1</h6>
+                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3 mb-3">
+                <div class="card">
+                    <img src="/img/produto_6.png" class="card-img-top">
+                    <div class="card-body">
+                        <h6 class="card-title">Produto 1</h6>
+                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3 mb-3">
+                <div class="card">
+                    <img src="/img/produto_7.jpeg" class="card-img-top">
+                    <div class="card-body">
+                        <h6 class="card-title">Produto 1</h6>
+                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3 mb-3">
+                <div class="card">
+                    <img src="/img/produto_8.jpg" class="card-img-top">
+                    <div class="card-body">
+                        <h6 class="card-title">Produto 1</h6>
+                        <a href="#" class="btn btn-sm btn-secondary">Adicionar item</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+</html>
