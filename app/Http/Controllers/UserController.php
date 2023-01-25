eb675ecb1a263ab84252deb6d8b94b269b23ee3b
RToramaru <rafael.almeida.soares2012@gmail.com>
2023-01-25 00:39:25 -0300

    mensagem de login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,12 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function login(Request $request)
     {
         $data = [];
+        if ($request->isMethod('post')) {
+            $this->validate($request, [
+                'email' => 'required|email',
+                'password' => 'required',
+            ]);
+            $credentials = $request->only('email', 'password');
+            if (Auth::attempt($credentials)) {
+                return redirect()->route('home');
+            } else {
+                return redirect()->route('login')->with('error', 'Email ou senha invalidos');
+            }
+        }
         return view('user.login', $data);
     }
 }
